2539ba212e0ab5d10ada73790d9b251fd4c7384c
Olga Panayot <panayot.olga@gmail.com>
2022-12-26 18:58:12 +0200

    update files

---
+++ src/js/api-pixabay.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+const API_KEY = '32359635-8d2ebe30e439ca8b9b1c02f68';
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+
+export class GalleryAPI {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  async axiosGet() {
+    const options = new URLSearchParams({
+      key: API_KEY,
+      q: this.searchQuery,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: 'true',
+      page: this.page,
+      per_page: 40,
+    });
+    const { data } = await axios(`?${options}`);
+    this.page += 1;
+    return data;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
